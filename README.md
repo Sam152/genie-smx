4554970f043427e400fae43beacdf54251cce268
Sam <54m152@gmail.com>
2022-05-30 22:41:45 +0800

    Add README and some formatting

---
+++ README.md
@@ -0,0 +1,11 @@
+genie-smx
+=========
+
+A parser for the [SMX file format](https://github.com/SFTtech/openage/blob/master/doc/media/smx-files.md) used by the genie engine, inspired by [genie-slp](https://github.com/genie-js/genie-slp).
+
+To build and test this project:
+
+```
+yarn install && tsc
+cd test && yarn install && yarn run watch
+```
--- test/src/index.ts
+++ test/src/index.ts
@@ -1,47 +1,44 @@
 import Smx, {PaletteCollectionFactory} from "genie-smx";
 
 async function createSmx() {
-  const palettes = await PaletteCollectionFactory.fromHttp('/static/palettes');
-  const asset = await fetch(`./static/u_arc_crossbowman_walkA_x2.smx`).then((response) => response.arrayBuffer())
-  const smx = new Smx(new Buffer(asset), palettes);
-  return smx;
+    const palettes = await PaletteCollectionFactory.fromHttp('/static/palettes');
+    const asset = await fetch(`./static/u_arc_crossbowman_walkA_x2.smx`).then((response) => response.arrayBuffer())
+    const smx = new Smx(new Buffer(asset), palettes);
+    return smx;
 }
 
 async function start() {
-  const smx = await createSmx();
+    const smx = await createSmx();
 
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-  const ctx = canvas.getContext('2d')!;
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d')!;
 
-  let frame = 10;
-  setInterval(async () => {
+    let frame = 10;
+    setInterval(async () => {
+        ctx.clearRect(0, 0, 800, 800);
 
-  ctx.clearRect(0,0,500,500);
+        const position = [150, 150];
 
-  const position = [150, 150];
+        const frameData = smx.getFrame(frame % smx.getFramesCount());
 
-  const frameData = smx.getFrame(frame % smx.getFramesCount());
+        const shadow = smx.renderShadow(frame % smx.getFramesCount());
+        const shadowBitmap = await createImageBitmap(shadow);
+        ctx.drawImage(
+            shadowBitmap,
+            position[0] - frameData.layers[1]!.centerX,
+            position[1] - frameData.layers[1]!.centerY,
+        );
 
-  const shadow = smx.renderShadow(frame % smx.getFramesCount());
-  const shadowBitmap = await createImageBitmap(shadow);
-    ctx.drawImage(
-        shadowBitmap,
-        position[0] - frameData.layers[1]!.centerX,
-        position[1] - frameData.layers[1]!.centerY,
-    );
+        const imageData = smx.renderFrame(frame % smx.getFramesCount(), 8);
+        const bitmap = await createImageBitmap(imageData);
+        ctx.drawImage(
+            bitmap,
+            position[0] - frameData.layers[0].centerX,
+            position[1] - frameData.layers[0].centerY,
+        );
 
-  const imageData = smx.renderFrame(frame % smx.getFramesCount(), 8);
-  const bitmap = await createImageBitmap(imageData);
-  ctx.drawImage(
-      bitmap,
-      position[0] - frameData.layers[0].centerX,
-      position[1] - frameData.layers[0].centerY,
-  );
-
-
-  frame++;
-
-  }, 20);
+        frame++;
+    }, 20);
 }
 
 start();
