67ba41e199ffc9109811b9b84e644e37e666747a
Sam <54m152@gmail.com>
2024-12-01 19:59:54 +0800

    perf

--- src/helper/createImageData.ts
+++ src/helper/createImageData.ts
@@ -1,5 +1,3 @@
-// @todo, could add support for a node.js version of this, @see genie-slp.
 export default function createImageData(width: number, height: number) {
-    const canvas = document.createElement('canvas');
-    return canvas.getContext('2d')!.createImageData(width, height);
+    return new ImageData(width, height);
 }
